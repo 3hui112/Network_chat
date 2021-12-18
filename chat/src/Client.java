50a8bff646ec2340c9f8152ed03d93b293dd4e6b
3hui112 <shinetheworld112@gmail.com>
2021-12-19 04:51:56 +0900

    commit

---
+++ chat/src/Client.java
@@ -0,0 +1,115 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+// 1. 사용자가 문자를 입력하고 이를 서버에 보내기
+// 2. 상대방이 서버에 보낸 문자를 받아서 채팅방에 출력해 주기 -> 사용자가 볼 수 있음(전해줌)
+// 
+
+// 키보드로 전송문자열 입력받아 서버로 전송하는 클래스
+class WriteThread{
+       Socket socket;
+       ClientFrame cf;
+       String str;
+       String id;
+       
+       public WriteThread(ClientFrame cf) {
+             this.cf  = cf;
+             this.socket= cf.socket;
+       }
+       
+       public void sendMsg() {
+             //키보드로부터 문자열을 읽어오기 위한 객체 생성
+             BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+             PrintWriter pw=null;
+
+             try{
+                    //서버로 문자열을 전송하기 위한 객체 생성
+                    pw=new PrintWriter(socket.getOutputStream(),true);
+
+                    //상대방에게 내 id와 함께 내 IP를 전송
+                    if(cf.isFirst==true){
+                           InetAddress iaddr=socket.getLocalAddress();                      
+                           String ip = iaddr.getHostAddress();                        
+                           getId();
+                           System.out.println("ip:"+ip+" id:"+id);
+                           str = "["+id+"]님 로그인 ("+ip+")";
+                    }else{
+                           str= "["+id+"] "+cf.txtF.getText();
+                    }
+                    
+                    //입력받은 문자열을 서버로 전송
+                    pw.println(str);
+            
+             }catch(IOException ie){
+                    System.out.println(ie.getMessage());
+             }finally{
+                    try{
+                           if(br!=null) br.close();
+                    }catch(IOException ie){
+                           System.out.println(ie.getMessage());
+                    }
+             }
+       }     
+
+       public void getId(){            
+             id = Id.getId();
+       }
+}
+
+//서버가 보내온 문자열을 전송받는 클래스
+class ReadThread extends Thread{
+       Socket socket;
+       ClientFrame cf;
+       
+       public ReadThread(Socket socket, ClientFrame cf) {
+             this.cf = cf;
+             this.socket=socket;
+       }
+
+       public void run() {
+             BufferedReader br = null;
+
+             try{
+                    //서버로부터 전송된 문자열 읽어오기 위한 객체 생성
+                    br=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    while(true){
+                           //소켓으로부터 문자열 읽어옴
+                           String str=br.readLine();
+                           if(str==null){
+                                 System.out.println("접속이 끊겼습니다.");
+                                 break;
+                           }
+                           //전송받은 문자열을 화면에 출력
+                           System.out.println("[확인용] " + str);
+                           cf.txtA.append(str+"\n");
+                    }
+             }catch(IOException ie){
+                    System.out.println(ie.getMessage());
+             }finally{
+                    try{
+                           if(br!=null) br.close();
+                           if(socket!=null) socket.close();
+                    }catch(IOException ie){}
+             }
+       }
+}
+
+public class Client {
+       public static void main(String[] args) {
+             Socket socket=null;
+             ClientFrame cf;
+             
+             try{
+                    socket=new Socket("125.240.97.109", 82);
+                    System.out.println("성공적으로 연결되었습니다.");
+                    cf = new ClientFrame(socket);
+                    new ReadThread(socket, cf).start();
+             }catch(IOException ie){
+                    System.out.println(ie.getMessage());
+             }
+       }
+}
---
+++ chat/src/Server.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+//사용자로부터 전송된 문자열을 받아 상대방에게 해당 문자열을 보내주는 클래스
+class EchoThread extends Thread{
+       Socket socket;
+       Vector<Socket> vec;
+       
+       public EchoThread(Socket socket, Vector<Socket> vec){
+             this.socket = socket;
+             this.vec = vec;
+       }
+       
+       public void run(){
+             BufferedReader br = null;
+             try{
+                    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String str = null;
+                    while(true){
+                           //사용자로부터 문자열 받기
+                           str=br.readLine();
+                           //상대방이 접속을 끊기면 벡터에서 제거하고 break;
+                           if(str==null){
+                                 vec.remove(socket);
+                                 break;
+                           }
+
+                           //연결된 소켓들을 통해서 다른 사용자에게 문자열 보내주기
+                           sendMsg(str);                   
+                    }
+             }catch(IOException ie){
+                    System.out.println(ie.getMessage());
+             }finally{
+                    try{
+                           if(br != null) br.close();
+                           if(socket != null) socket.close();
+                    }catch(IOException ie){
+                           System.out.println(ie.getMessage());
+                    }
+             }
+       }
+
+
+       //전송받은 문자열 다른 사용자들에게 보내주는 메서드
+       public void sendMsg(String str){
+             try{
+                 //현재의 소켓이 데이터를 보낸 동일한 사용자인 경우를 제외하고 나머지 소켓들에게만 데이터를 전송하는 for문
+                    for(Socket socket:vec){
+                           if(socket != this.socket){
+                                 PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+                                 pw.println(str);
+                                 pw.flush();
+                           }
+                    }
+             } catch(IOException ie){
+                    System.out.println(ie.getMessage());
+             }
+       }
+}
+
+public class Server {
+       public static void main(String[] args) {
+             ServerSocket server = null;
+             Socket socket =null;
+             //사용자와 연결된 소켓들을 배열처럼 저장할 벡터 객체 생성
+             Vector<Socket> vec = new Vector<Socket>();
+             
+             try{
+                    server = new ServerSocket(82);
+                    while(true){
+                           System.out.println("대기 중입니다.");
+                           socket = server.accept();
+                           //사용자와 연결된 소켓을 벡터에 저장
+                           vec.add(socket);
+                           
+                           new EchoThread(socket, vec).start(); //위에서 선언한 클래스의 객체 생성
+                    }
+             }catch(IOException ie){
+                    System.out.println(ie.getMessage());
+             }
+       }
+}
